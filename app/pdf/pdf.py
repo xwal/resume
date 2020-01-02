4386428134704ed985fa4a777ba1b2a8973bb769
Chaosky <chaosky.me@gmail.com>
2020-01-02 11:23:07 +0800

    update resume

--- app/pdf/pdf.py
+++ app/pdf/pdf.py
@@ -55,6 +55,13 @@ def apply_two_column (pdf_instance, arr, draw_func):
             heights.append(pdf_instance.get_y())
         top = max(heights)
         i = i + 2
+# single column function
+def apply_single_column (pdf_instance, arr, draw_func):
+    top = pdf_instance.get_y()
+    for item in arr:
+        left = page_size['left'] + page_size['section_indent']
+        draw_func(pdf_instance, item, left, top)
+        top = pdf_instance.get_y()
 
 # draw function for employment
 def draw_func_employment(pdf_instance, job, left, top):
@@ -85,11 +92,12 @@ def draw_func_project(pdf_instance, proj, left, top):
     pdf_instance.cell(1, line_height['text'], proj['name'] + u' (' + proj['year'] + ')', ln=2)
     pdf_instance.set_font_size(font_size['text'])
     pdf_instance.set_text_color(black)
-    pdf_instance.cell(1, line_height['text'] + 1, u'[ ' + u', '.join(proj['library']) + ' ]', ln=2)
+    pdf_instance.multi_cell(0, line_height['text'] + 1, u'[ ' + u', '.join(proj['library']) + ' ]')
     for desc in proj['description']:
+        pdf_instance.set_x(left)
         pdf_instance.ellipse(pdf_instance.get_x() + 1, pdf_instance.get_y() + 1.5, ellipse['size'], ellipse['size'], 'F')
         pdf_instance.cell(ellipse['margin'])
-        pdf_instance.cell(1, line_height['text'], desc, ln=2)
+        pdf_instance.multi_cell(0, line_height['text'], desc, align="L")
         pdf_instance.set_x(left)
     pdf_instance.ln(page_size['section_margin'])
 
@@ -166,7 +174,6 @@ for lang in all_langs:
     pdf.set_font(text_font, '', font_size['text'])
     for univ in data['education']['universities']:
         pdf.set_x(page_size['left'] + page_size['section_indent'])
-        univ_y = pdf.get_y()
         pdf.set_font_size(font_size['text'] + 1)
         pdf.set_text_color(*brown)
         pdf.cell(1, line_height['text'], univ['name'], ln=2)
@@ -176,20 +183,6 @@ for lang in all_langs:
         pdf.set_text_color(grey)
         pdf.cell(1, line_height['text'] - 1, univ['location'] + u' -- ' + univ['year'], ln=2)
         pdf.set_text_color(black)
-
-        pdf.set_y(univ_y)
-        for item in univ['items']:
-            pdf.set_x(page_size['middle_x'])
-            pdf.ellipse(pdf.get_x(), pdf.get_y() + 1.5, ellipse['size'], ellipse['size'], 'F')
-            pdf.cell(2)
-            if 'link' in item:
-                pdf.cell(pdf.get_string_width(item['textBefore']), line_height['text'] - 1, item['textBefore'], ln=0)
-                pdf.set_text_color(*blue)
-                pdf.cell(pdf.get_string_width(item['link']['text']), line_height['text'] - 1, item['link']['text'], ln=0, link=item['link']['url'])
-                pdf.set_text_color(0)
-                pdf.cell(1, line_height['text'], item['textAfter'], ln=2)
-            else:
-                pdf.cell(1, line_height['text'], item['text'], ln=2)
     pdf.ln(page_size['section_margin'])
     # employment section
     pdf.set_x(page_size['left'])
@@ -209,30 +202,13 @@ for lang in all_langs:
         skill_names = [i['name'] for i in item['skills']]
         pdf.cell(1, line_height['text'] - 1, item['text'] + u': ' + ', '.join(skill_names), ln=2)
         pdf.ln(page_size['section_margin'])
-    pdf.set_x(page_size['left'] + page_size['section_indent'])
-    pdf.ellipse(pdf.get_x(), pdf.get_y() + 1.5, ellipse['size'], ellipse['size'], 'F')
-    pdf.cell(ellipse['margin'])
-    pdf.cell(1, line_height['text'] - 1, data['skill']['library']['text'] + u': ' + ', '.join(data['skill']['library']['children']), ln=2)
-    pdf.ln(page_size['section_margin'])
-    pdf.set_x(page_size['left'] + page_size['section_indent'])
-    pdf.ellipse(pdf.get_x(), pdf.get_y() + 1.5, ellipse['size'], ellipse['size'], 'F')
-    pdf.cell(ellipse['margin'])
-    pdf.cell(1, line_height['text'] - 1, data['skill']['tooling']['text'] + u': ' + ', '.join(data['skill']['tooling']['children']), ln=2)
     pdf.ln(page_size['section_margin'])
     # project section
     pdf.set_x(page_size['left'])
     pdf.set_font(title_font, '', font_size['section_title'])
-    pdf.cell(pdf.get_string_width(data['sections']['project']), line_height['section'] - 1, data['sections']['project'], ln=0)
+    pdf.cell(1, line_height['section'] - 1, data['sections']['project'], ln=2)
     pdf.set_font(text_font, '', font_size['text'])
-    pdf.set_text_color(grey)
-    pdf.set_y(pdf.get_y() + 2)
-    pdf.cell(30)
-    pdf.cell(pdf.get_string_width(data['pdf']['github']['text']), line_height['text'] - 1, data['pdf']['github']['text'], ln=0)
-    pdf.set_text_color(*blue)
-    pdf.cell(1, line_height['text'] - 1, data['about']['socials']['github']['url'], ln=2, link=data['about']['socials']['github']['url'])
-    pdf.set_y(pdf.get_y() + 3)
-    pdf.set_text_color(black)
     projects = data['project']['col1'] + data['project']['others']['repos']
-    apply_two_column(pdf, projects, draw_func_project);
+    apply_single_column(pdf, projects, draw_func_project);
     # output file
     pdf.output('./dist/' + data['badge']['pdfLink'], 'F')
